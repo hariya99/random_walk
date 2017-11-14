3f8ad43275f92efa5e722b2151be7e786d2c8406
Harish Chauhan <harish.chauhan99@gmail.com>
2017-11-14 14:41:27 +0530

    Update rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -5,5 +5,25 @@ from random_walk import RandomWalk
 rw = RandomWalk()
 rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
+#plt.scatter(rw.x_values, rw.y_values, s=15)
+
+#creating a colorful scatter
+point_numbers = list(range(rw.num_points))
+plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+
+#emphasize the first and last point
+plt.scatter(0,0,c='green', edgecolor='none',s=100)
+plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
+
+#use the below code if you want to remove axes
+#plt.axes().get_xaxis().set_visible(False)
+#plt.axes().get_yaxis().set_visible(False)
+
+#use the below code to resize the window 
+#plt.figure(figsize=(10,6))
+
+#use the below code to set pixels for graph 
+#plt.figure(dpi=128, figsize=(10,6))
+
+#show the plot
 plt.show()
